a6d1b6c58b9fe34332a3cd9a60adb5f609b6aaf5
TuF3i <13759301652@163.com>
2026-08-03 02:16:29 +0800

    ✨ 26-8-3

--- plugins/redrock_caidanci/main.lua
+++ plugins/redrock_caidanci/main.lua
@@ -412,7 +412,7 @@ function on_message(event)
 
     -- 没有游戏时：检查引导触发词
     if not get_game(event.group_id) then
-        local triggers = { "无聊", "没意思", "没劲", "有啥好玩", "有什么好玩", "好玩的", "玩什么" }
+        local triggers = { "没意思", "没劲", "有啥好玩", "有什么好玩", "好玩的", "玩什么" }
         for _, kw in ipairs(triggers) do
             if raw:find(kw, 1, true) then
                 reply(event, "来玩猜单词呀！@卷娘 /猜单词 就能开局～\n看看你能用几次猜出正确答案 🤔")
